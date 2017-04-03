b6b9a5a44b8097ddb4e41582adb13b63d12022e0
Jasper de Meijer <Jasperdemeijer@gmail.com>
2017-04-03 12:21:22 +0200

    push data import/export

--- src/orffinder/DataExporter.java
+++ src/orffinder/DataExporter.java
@@ -5,10 +5,40 @@
  */
 package orffinder;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 /**
  *
- * @author jdm
+ * @author 
  */
 public class DataExporter {
+
+    static private Connection connection;
+    public static Connection
+
+    getConnection() throws Exception {
+        if (connection == null) {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/xirect.nl", "pg16", "blaat1234_16");
+        }
+        return connection;
+    }
+
+    public void exportData(int seqID, String originNseq, String projectName,
+            String Organism, String header, int ORFID, String ORFNSeq, int RFNumber) {
+        String query1 = "insert into items values ('"+seqID+"','"+originNseq+"','"+projectName+"','"+Organism+"','"+header+"')";
+        String query2 = "insert into items values ('"+ORFID+"','"+ORFNSeq+"','"+RFNumber+"','"+seqID+"')";
+        
+        try{
+            Connection c = getConnection();
+            Statement stmt = c.createStatement();
+            stmt.executeUpdate(query1);
+            stmt.executeUpdate(query2);
+        }catch(Exception e){
+            System.out.println("something went wrong");
+        }
+    }
     
 }
