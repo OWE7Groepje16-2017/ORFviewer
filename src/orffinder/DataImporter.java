b08ed63640ad720ce5348d880fb410ffcc0d7f90
TIM <timvandekerkhof@hotmail.com>
2017-04-03 15:58:32 +0200

    added importer

--- src/orffinder/DataImporter.java
+++ src/orffinder/DataImporter.java
@@ -5,20 +5,46 @@
  */
 package orffinder;
 
+import java.sql.*;
+import java.util.ArrayList;
+
+
 /**
  *
  * @author jdm
  */
 public class DataImporter {
     
-    public void importDatabase(){
-        try{
+    static private Connection connection = null;
+    private ArrayList<String> importedData = new ArrayList();
+
+    public static Connection getConnection() throws Exception {
+        if (connection == null) {
             Class.forName("com.mysql.jdbc.Driver");
-            System.out.println("driverloaded!");
-        }catch (ClassNotFoundException e){
-            e.printStackTrace();
+            connection = DriverManager.getConnection("jdbc:mysql://xirect.nl:3306/pg16", "pg16", "blaat1234_16");
         }
+        return connection;
+    }
+    public ArrayList<String> importData(String projectName){
         
+        try {
+            Connection c = getConnection();
+            Statement getSequences = c.createStatement();
+            String seqQuery = "SELECT originNSeq as obtSeq, header as obtHeader FROM sequence "
+                    + "WHERE projectName = '"+projectName+"';";
+            ResultSet rs = getSequences.executeQuery(seqQuery);
+            while (rs.next()) {
+                importedData.add(rs.getString("obtHeader"));
+                importedData.add(rs.getString("obtSeq"));
+            }
+        } catch (Exception e) {
+            System.out.println("something went wrong while fetching maxORF");
+            e.printStackTrace();
+        
+
+        
+    }
+        return importedData;
     }
     
 }
